0f48f32d12293ba85fd9b136d78eac908f994589
youngjuKim <kjuzoojuk@naver.com>
2025-02-05 17:32:34 +0900

    feat: 백준 1427 소트인사이드

---
+++ src/정렬/선택정렬/백준_1427_소트인사이드.java
@@ -0,0 +1,51 @@
+package 정렬.선택정렬;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class 백준_1427_소트인사이드 {
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        // input
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String nStr = br.readLine();
+        int len = nStr.length();
+        arr = new int[len];
+        int k = 0;
+        for(String s : nStr.split("")){
+            arr[k++] = Integer.parseInt(s);
+        }
+
+        // process
+        int sIdx = 0;
+        while (sIdx < len) {
+            int maxIdx = searchMaxIdx(sIdx, len);
+            swap(maxIdx, sIdx++);
+        }
+
+        // output
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<len;i++){
+            sb.append(arr[i]);
+        }
+        System.out.println(sb);
+    }
+
+    static void swap(int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    static int searchMaxIdx(int sIdx, int eIdx) {
+        int max = Integer.MIN_VALUE, maxIdx = 0;
+        for (int i = sIdx; i < eIdx; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+                maxIdx = i;
+            }
+        }
+        return maxIdx;
+    }
+}
