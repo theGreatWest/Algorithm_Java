67286462d7ef56d882993be594b732437cb03b39
youngjuKim <kjuzoojuk@naver.com>
2025-02-06 15:41:34 +0900

    feat: 백준 1927 최소 힙 ( Day 1 ) -> 우선 순위 정렬 기준을 절댓값의 가장 작은 값/큰 값으로 확장해 다시 풀어봄

--- src/매일1문제/Date_25_02_06/백준_1927_최소힙.java
+++ src/매일1문제/Date_25_02_06/백준_1927_최소힙.java
@@ -9,8 +9,27 @@ public class 백준_1927_최소힙 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        // process
+        // original process : 작은 값 부터 정렬되어야 한다면?
         PriorityQueue<Integer> pq = new PriorityQueue<>(); // 기본적으로 작은 값부터 나오게 되어있음
+
+        // + process 1 : 만약 절댓값 기준 작은 값부터 정렬되어야 한다면?
+//        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+//            int absO1 = Math.abs(o1);
+//            int absO2 = Math.abs(o2);
+//
+//            if(absO1==absO2) return o1-o2;
+//            else return absO1 - absO2;
+//        });
+
+        // + process 2 : 만약 절댓값 기준 큰 값부터 정렬되어야 한다면?
+//        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+//            int absO1 = Math.abs(o1);
+//            int absO2 = Math.abs(o2);
+//
+//            if(absO1==absO2) return o2-o1;
+//            else return absO2-absO1;
+//        });
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             int x = Integer.parseInt(br.readLine());
