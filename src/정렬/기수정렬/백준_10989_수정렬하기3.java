43f89f8b3b8c2740a69e1366f5eb72482ab66f52
youngjuKim <kjuzoojuk@naver.com>
2025-02-09 15:36:28 +0900

    feat: 백준 10989 수 정렬하기3 연습 -> 공부

--- src/정렬/기수정렬/백준_10989_수정렬하기3.java
+++ src/정렬/기수정렬/백준_10989_수정렬하기3.java
@@ -1,6 +1,7 @@
 package 정렬.기수정렬;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class 백준_10989_수정렬하기3 {
     static int[] arr;
@@ -32,29 +33,38 @@ public class 백준_10989_수정렬하기3 {
 
     static void radixSort(int maxSize) {
         int[] processedArr = new int[arr.length]; // 정렬 결과를 저장할 배열
-        int pointer = 1; // 자릿수
+        int pointer = 1; // 자릿수( 1의 자리부터 시작해서 10단위로 증가 )
 
-        for (int i = 0; i < maxSize; i++) { // 최대 자릿수 만큼 반복(자릿수 기준 저장 -> 정렬)
-            int[] numbers = new int[10]; // 들어갈 수 있는 숫자: 0~9
+        for (int i = 0; i < maxSize; i++) { // 최대 자릿수 만큼 반복( Ex. 최댓값이 1110이면 4번 반복 )
+            int[] numbers = new int[10]; // 0~9의 숫자가 몇 번 나왔는지 카운트
 
+            // [ 1 단계 ] 현재 자릿수를 기준으로 각 숫자의 개수 세기
             for (int j = 0; j < arr.length; j++) {
                 numbers[arr[j] / pointer % 10]++; // index와 해당 자리의 값이 같다면 +1
             }
 
+            // [ 2 단계 ] 누적합 계산 -> 실제로 정렬될 위치 파악을 위해
             for (int j = 1; j < 10; j++) {
                 numbers[j] += numbers[j - 1]; // 합 배열(누적합) 이용해 index 계산
             }
 
-            // 현재 자릿수 기준으로 정렬
+            // [ 3 단계 ] 현재 자릿수를 기준으로 정렬( 역순으로 순회 )
             for (int j = arr.length - 1; j >= 0; j--) {
-                processedArr[numbers[(arr[j] / pointer % 10)] - 1] = arr[j];
-                numbers[(arr[j] / pointer) % 10]--;
+                int index = arr[j] / pointer % 10;
+                processedArr[numbers[index] - 1] = arr[j];
+                numbers[index]--;
+                // 이 단계에서 굳이 같은 기수끼리 정렬할 필요 없다( 순서대로 그냥 내보내기 )
+                System.out.print("processedArr = " + Arrays.toString(processedArr));
+                System.out.print(" / numbers = " + Arrays.toString(numbers)+"\n");
             }
 
+            // [ 4 단계 ] 현재 자릿수 기준으로 정렬된 결과를 원본 배열에 복사
             for (int j = 0; j < arr.length; j++) {
                 arr[j] = processedArr[j]; // 다음 자릿수로 이동하기 위해 현재 자릿수 기준 정렬 데이터 저장
             }
-            pointer *= 10; // 자릿수 증가 시키기
+
+            // [ 5 단계 ] 자릿수 증가
+            pointer *= 10;
         }
     }
 }
