f072b8f98c198ace3fff65d1be8beddd46f2af46
youngjuKim <kjuzoojuk@naver.com>
2025-02-17 09:04:39 +0900

    feat: 백준 10828 스택

---
+++ src/DailyCheckUp/Date_25_02_17/백준_10828_스택.java
@@ -0,0 +1,30 @@
+package DailyCheckUp.Date_25_02_17;
+
+import java.util.*;
+import java.io.*;
+
+public class 백준_10828_스택 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            if (input.length == 2) {
+                stack.push(Integer.parseInt(input[1]));
+            } else {
+                String order = input[0];
+                if (order.equals("pop")) bw.write((stack.isEmpty()) ? "-1" : String.valueOf(stack.pop()));
+                else if (order.equals("size")) bw.write(String.valueOf(stack.size()));
+                else if (order.equals("empty")) bw.write((stack.isEmpty()) ? "1" : "0");
+                else bw.write((stack.isEmpty()) ? "-1" : String.valueOf(stack.peek()));
+                bw.write("\n");
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
