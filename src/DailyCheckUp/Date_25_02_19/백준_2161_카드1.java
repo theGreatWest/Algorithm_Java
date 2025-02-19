d4299308af07307685ed5445cc10e6e9a13b635f
youngjuKim <kjuzoojuk@naver.com>
2025-02-19 10:58:40 +0900

    feat: 백준 2161 카드1

---
+++ src/DailyCheckUp/Date_25_02_19/백준_2161_카드1.java
@@ -0,0 +1,24 @@
+package DailyCheckUp.Date_25_02_19;
+
+import java.util.*;
+import java.io.*;
+
+public class 백준_2161_카드1 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        Deque<Integer> d = new LinkedList<>();
+        for(int i=1;i<=n;i++){
+            d.offer(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!d.isEmpty()){
+            sb.append(d.poll()).append(" ");
+            if(d.size()>=2) d.offer(d.poll());
+        }
+
+        System.out.println(sb);
+    }
+}
