796d16b0d55809e5279b289a7be7175da93c1a7a
youngjuKim <kjuzoojuk@naver.com>
2025-03-03 08:43:30 +0900

    feat: 백준 11047 동전O

---
+++ src/DailyCheckUp/Date_25_03_03/백준_11047_동전O.java
@@ -0,0 +1,26 @@
+package DailyCheckUp.Date_25_03_03;
+
+import java.util.*;
+import java.io.*;
+
+public class 백준_11047_동전O {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        for(int i=n-1;i>=0;i--){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int cnt = 0;
+        for(int i=0; ( i<n || k!=0 );i++){
+            cnt += (k/arr[i]);
+            k %= arr[i];
+        }
+
+        System.out.println(cnt);
+    }
+}
