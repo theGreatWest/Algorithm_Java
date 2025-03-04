54b1c2e24d85b67cad50cbb6a05da307982075af
youngjuKim <kjuzoojuk@naver.com>
2025-03-04 10:38:25 +0900

    feat: 백준 12851 숨바꼭질 (2) : 경로의 가짓수 찾기 메서드 추가

--- src/DailyCheckUp/Date_25_03_04/백준_12851_숨바꼭질2.java
+++ src/DailyCheckUp/Date_25_03_04/백준_12851_숨바꼭질2.java
@@ -31,7 +31,7 @@ public class 백준_12851_숨바꼭질2 {
         backTrace(k, new ArrayList<>(), paths);
 
         System.out.println(minSec);
-        for(List<Integer> path : paths){
+        for (List<Integer> path : paths) {
             System.out.println(path);
         }
 
@@ -69,7 +69,8 @@ public class 백준_12851_숨바꼭질2 {
                     depth[next] = depth[current] + 1; // depth +1 증가시키기
                     prevNode[next].add(current); // next 의 이전 노드가 current 임을 저장
                     q.offer(next);
-                } else if (depth[next] == depth[current] + 1) prevNode[next].add(current); // 이미 방문한 노드인 경우, 다음 노드의 깊이가 현재 노드의 깊이 + 1과 같을 때, 바로 이전 노드라고 판단할 수 있다.
+                } else if (depth[next] == depth[current] + 1)
+                    prevNode[next].add(current); // 이미 방문한 노드인 경우, 다음 노드의 깊이가 현재 노드의 깊이 + 1과 같을 때, 바로 이전 노드라고 판단할 수 있다.
             }
         }
     }
@@ -90,6 +91,35 @@ public class 백준_12851_숨바꼭질2 {
         path.remove(path.size() - 1); // 백트래킹
     }
 
+// 경로의 가짓수 찾기
+    static int[] ways;
+
+    static void bfs_cnt() {
+        depth = new int[MAX];
+        Arrays.fill(depth, -1); // int[], double[] 일 때만 사용 가능. 자료형이 객체일 때 사용하면 절대 NO!!
+
+        ways = new int[MAX];
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(n);
+        depth[n] = 0;
+        ways[n] = 1;
+
+        while (!q.isEmpty()) {
+            int current = q.poll();
+
+            for (int next : new int[]{current - 1, current + 1, current * 2}) {
+                if (isOutOfRange(next)) continue;
+
+                if (depth[next] == -1) { // 아직 방문하지 않은 노드
+                    depth[next] = depth[current] + 1; // depth +1 증가시키기
+                    ways[next] = ways[current]; // next 의 이전 노드가 current 임을 저장
+                    q.offer(next);
+                } else if (depth[next] == depth[current] + 1) ways[next] += ways[current]; // 이전의 모든 노드들의 경로 값을 더해줘야 하기 때문에
+            }
+        }
+    }
+
 
     // 내장 Class 를 이용한 방법 : 최단 거리(최단 depth)만 구할 수 있다.
     static void bfs() {
@@ -100,12 +130,6 @@ public class 백준_12851_숨바꼭질2 {
         while (!q.isEmpty()) {
             Node curr = q.poll();
             int currNum = curr.num, currDepth = curr.depth;
-
-
-            if (currNum == k) {
-                if (minSec == 0) minSec = currDepth;
-                if (minSec == currDepth) way++;
-            }
 
             for (int next : new int[]{currNum - 1, currNum + 1, 2 * currNum}) {
                 if (!isOutOfRange(next) && !visited[next]) {
